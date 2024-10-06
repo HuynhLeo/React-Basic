c657a9dcc0da477c5b309358b0f157bf56e33cfd
Le Huynh Nhan <lhnhan07@gmail.com>
2024-10-06 21:57:34 +0700

    video16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render(){
+        return(
+            <div>
+                Leo đẹp trai
+                {Math.random()}
+            </div>
+        )
+    }
+}
+export default MyComponent;
