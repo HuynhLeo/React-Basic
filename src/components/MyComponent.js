66724b44842f16e4a5cd472b268915d064f279c5
Le Huynh Nhan <lhnhan07@gmail.com>
2024-10-07 10:06:05 +0700

    video20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,19 +6,24 @@ class MyComponent extends React.Component {
         address: 'Cao Lanh',
         age: '21'
     };
-    handleClick(event) {
+    handleClick = (event) => {
         console.log('Leo dz')
         console.log(' My name is', this.state.name)
+        this.setState({
+            name: 'Leo1',
+            age: 18
+        })
     }
     handleOnMoverOver(event) {
         console.log(event.pageX)
+
     }
     // JSX
     render() {
         return (
             <div>
                 {/* Leo đẹp trai */}
-                My name is {this.state.name} and I'm from {this.state.address}
+                My name is {this.state.name} and I'm {this.state.age}
                 <div>
                     <button onClick={this.handleClick}>Click me</button>
                     <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
