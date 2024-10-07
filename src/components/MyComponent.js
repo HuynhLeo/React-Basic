3dee600c2de36f01c0efbd11fabe2b2d65963339
Le Huynh Nhan <lhnhan07@gmail.com>
2024-10-07 09:55:02 +0700

    video19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,13 +6,23 @@ class MyComponent extends React.Component {
         address: 'Cao Lanh',
         age: '21'
     };
-
+    handleClick(event) {
+        console.log('Leo dz')
+        console.log(' My name is', this.state.name)
+    }
+    handleOnMoverOver(event) {
+        console.log(event.pageX)
+    }
     // JSX
     render() {
         return (
             <div>
                 {/* Leo đẹp trai */}
                 My name is {this.state.name} and I'm from {this.state.address}
+                <div>
+                    <button onClick={this.handleClick}>Click me</button>
+                    <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
+                </div>
             </div>
         )
     }
