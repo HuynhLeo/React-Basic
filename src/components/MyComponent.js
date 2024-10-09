0d8d7ba84e2aae904d3d60590d60b636b5e99801
Le Huynh Nhan <lhnhan07@gmail.com>
2024-10-09 23:09:39 +0700

    props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,13 +1,21 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
 
     // JSX
     render() {
+        const arr = [
+            'a', 'b', 'c'
+        ]
         return (
             <div>
                 <UserInfor />
+                <hr />
+                <DisplayInfor name='Leo' age={21} />
+                <hr />
+                <DisplayInfor name='dep trai' age='22' arr={arr} />
             </div>
 
         )
